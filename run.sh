fbad7ed3286db267944c01682283903c3f51290a
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-11-14 19:14:48 +0900

    feat: ブート成功

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 set -xue
 QEMU=qemu-system-riscv32
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
 
+CC=clang # clangのパス
 
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c # カーネルをビルド
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot\
+    -kernel kernel.elf
 # qemu-system-riscv32 -machine virt -bios default -nographic -serial mon:stdio --no-rebootだとエラーが出る
 # QEMUが-bios defaultで呼び出すファームウェアが本の執筆当時とは変わっているっぽい
 # riscv-gnu-toolchainとQEMUをソースコードからビルドして対処
